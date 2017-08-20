8c20f62305041b94095d13656c9c7bfdbc6bd68f
Scott <scottsaunders@mac.com>
2017-08-20 13:56:09 -0400

    added minfied version of scripts

--- magazine_v3/includes/big-media-link.php
+++ magazine_v3/includes/big-media-link.php
@@ -2,11 +2,11 @@
 						
 	<?php $selectMp4Source = get_sub_field('mp4_source'); if ($selectMp4Source == "url"):?>
 	      	<img class="big-play dark-btn movie-btn <?php the_sub_field('select');?>" vidurl="<?php the_sub_field('mp4_remote_source');?>" src="<?php bloginfo('template_directory'); ?>/images/ic_play_circle_outline_black_24px.svg">
-			<img class="big-play lt-btn movie-btn <?php the_sub_field('select');?>" vidurl="<?php the_sub_field('mp4_upload');?>" src="<?php bloginfo('template_directory'); ?>/images/ic_play_circle_outline_white_24px.svg">
+			<img class="big-play lt-btn movie-btn <?php the_sub_field('select');?>" vidurl="<?php the_sub_field('mp4_remote_source');?>" src="<?php bloginfo('template_directory'); ?>/images/ic_play_circle_outline_white_24px.svg">
 	<?php endif; ?>
 	
 	<?php $selectMp4Source = get_sub_field('mp4_source'); if ($selectMp4Source == "upload"):?>
-	 		<img class="big-play dark-btn movie-btn <?php the_sub_field('select');?>" vidurl="<?php the_sub_field('mp4_remote_source');?>" src="<?php bloginfo('template_directory'); ?>/images/ic_play_circle_outline_black_24px.svg">
+	 		<img class="big-play dark-btn movie-btn <?php the_sub_field('select');?>" vidurl="<?php the_sub_field('mp4_upload');?>" src="<?php bloginfo('template_directory'); ?>/images/ic_play_circle_outline_black_24px.svg">
 	      	<img class="big-play lt-btn movie-btn <?php the_sub_field('select');?>" vidurl="<?php the_sub_field('mp4_upload');?>" src="<?php bloginfo('template_directory'); ?>/images/ic_play_circle_outline_white_24px.svg">
 	<?php endif; ?>
 	
