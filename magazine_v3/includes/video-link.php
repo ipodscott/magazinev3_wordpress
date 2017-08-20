d77e973d67dc53ba3d47b52e27009f7ea8eb2152
Scott <scottsaunders@mac.com>
2017-08-20 13:58:44 -0400

    added the abilty to select custom image for youtube video or pull defalt youtube poster

--- magazine_v3/includes/video-link.php
+++ magazine_v3/includes/video-link.php
@@ -6,9 +6,21 @@
 	<div class="vid-link movie-btn <?php the_sub_field('select_video_size'); ?>" vidurl="<?php $vidURL = get_sub_field('select_video_file'); if ($vidURL == "text"):?><?php the_sub_field('video_text_url');?><?php endif; ?><?php $vidURL = get_sub_field('select_video_file'); if ($vidURL == "upload"):?><?php the_sub_field('video_upload');?><?php endif; ?>">
 <?php endif; ?>
 											
-											
-									
-<img src="<?php the_sub_field('video_thumb');?>">
+
+<?php $vidFormat = get_sub_field('youtube_thumb'); if ($vidFormat == "default"):?> 
+
+	<?php $vidFormat = get_sub_field('select_video_format'); if ($vidFormat == "youtube"):?> <img src="https://img.youtube.com/vi/<?php the_sub_field('youtube');?>/hqdefault.jpg"> <?php endif; ?>
+
+<?php endif; ?>
+
+<?php $vidFormat = get_sub_field('youtube_thumb'); if ($vidFormat == "custom"):?> 
+
+	<?php $vidFormat = get_sub_field('select_video_format'); if ($vidFormat == "youtube"):?> <img src="<?php the_sub_field('video_thumb');?>"> <?php endif; ?>
+
+<?php endif; ?>
+
+<?php $vidFormat = get_sub_field('select_video_format'); if ($vidFormat == "mp4"):?> <img src="<?php the_sub_field('video_thumb');?>"> <?php endif; ?>
+							
 	<div class="vid-overlay">
 		<div class="vid-table">
 			<div class="cell"><i class="material-icons">play_circle_outline</i></div>
